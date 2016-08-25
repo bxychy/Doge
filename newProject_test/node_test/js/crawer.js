be50fb45a82496e3bc5747d26ad2a9471bff68e2
zsychy <984249214@qq.com>
2016-08-25 23:50:01 +0800

    node-打卡日常

--- newProject_test/node_test/js/crawer.js
+++ newProject_test/node_test/js/crawer.js
@@ -1,32 +1,20 @@
 var http=require('http');
-var Promise=require('bluebird');
 var cheerio=require('cheerio');
 //var _url='http://www.baidu.com/';
 var _url='http://www.imooc.com/learn/348';
-var baseUrl='http://www.imooc.com/learn/';
-var videoIds=[348,259,75];
+
 
 function filterChapters(_html){
 	var $=cheerio.load(_html);
 	var chapters=$('.chapter');
-	var title=$('#page_hader .path span').text();
-	var numbers=parseInt($($('.info_num i')[0]).text().trim(),10);
-//	courseData={
-//		title:title,
-//		numbers:numbers,
-//		videos:[{
-//			chapterTitle:'',
-//			videos:[
-//				title:'',
-//				id:''
-//			]
-//		}]
-//	}
-	var courseData={
-		videos:[],
-		numbers:numbers,
-		title:title,
-	};
+//	[{
+//		chapterTitle:'',
+//		videos:[
+//			title:'',
+//			id:''
+//		]
+//	}]
+	var courseData=[];
 	chapters.each(function(item){
 		var chapter=$(this);
 		var chapterTitle=chapter.find('strong').text();
@@ -44,61 +32,28 @@ function filterChapters(_html){
 				id:id
 			});
 		});
-		courseData.videos.push(chapterData);
+		courseData.push(chapterData);
 	});
 	return courseData;
 }
-function printCourseInfo(courseDatas){
-	courseDatas.forEach(function(courseData){
-		console.log(courseData.number+'人学过'+courseData.title+'\n');
-	})
-	courseDatas.forEach(function(courseData){
-		console.log('####'+courseData.title+'\n');
-		courseData.forEach(function(item){
-			var chapterTitle=item.chapterTitle;
-			console.log(chapterTitle+'\n');
-			item.videos.forEach(function(video){
-				console.log('id:['+video.id+']'+video.title+'/n');
-			});
+function printCourseInfo(courseData){
+	courseData.forEach(function(item){
+		var chapterTitle=item.chapterTitle;
+		console.log(chapterTitle+'\n');
+		item.videos.forEach(function(video){
+			console.log('id:['+video.id+']'+video.title+'/n');
 		});
 	});
 }
-
-function getPageAsync(url){
-	return new Promise(function(resolve,reject){
-		console.log('正在爬取'+url);
-		http.get(_url,function(res){
-			var _html='';
-			res.on('data',function(data){
-				_html+=data;
-			});
-			res.on('end',function(){
-				resolve(_html);
-//				var courseData=filterChapters(_html);
-//				printCourseInfo(courseData);
-			});
-		}).on('error',function(e){
-			reject(e)
-			console.log('获取百度数据错误');
-		});
-	})
-}
-
-var fetchCourseArray=[];
-
-videoIds.forEach(function(id){
-	fetchCourseArray.push(getPageAsync(baseUrl+id));
-});
-
-Promise.all(fetchCourseArray).then(function(pages){
-	var courseDatas=[];
-	pages.forEach(function(html){
-		var courese=filterChapters(html);
-		courseDatas.push(courese);
+http.get(_url,function(res){
+	var _html='';
+	res.on('data',function(data){
+		_html+=data;
 	});
-	courseDatas.sort(function(x,y){
-		return x.numbers<y.numbers
+	res.on('end',function(){
+		var courseData=filterChapters(_html);
+		printCourseInfo(courseData);
 	});
-	printCourseInfo();
+}).on('error',function(){
+	console.log('获取百度数据错误');
 });
-
