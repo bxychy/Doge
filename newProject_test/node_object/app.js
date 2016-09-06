2d9c7a558f0595d0994d24d2d1d9350d78b09141
zsychy <984249214@qq.com>
2016-09-06 17:47:29 +0800

    vue/node-日常打卡

--- newProject_test/node_object/app.js
+++ newProject_test/node_object/app.js
@@ -11,28 +11,56 @@ console.log('imooc start on port'+port);
 // index page
 app.get('/',function(x,y){
 	y.render('index',{
-		title:'biu~ 首页'
+		title:'biu~ 首页',
+		movies[{
+			title:'机器管家',
+			_id:1,
+			poster:'https://www.baidu.com'
+		}]
 	});
 });
 
 // datile page
 app.get('/movie/:id',function(x,y){
 	y.render('datile',{
-		title:'biu~ 详情页'
+		title:'biu~ 详情页',
+		movie:{
+			doctor:'bbb',
+			country:'china',
+			title:'机器人',
+			year:'2222',
+			poster:'http://www.baidu.com',
+			language:'中文',
+			flash:'http:player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary:'mdzzyaoshangtian'
+		}
 	});
 });
 
 // admin page
 app.get('/admin/movie',function(x,y){
 	y.render('admin',{
-		title:'biu~ 后台'
+		title:'biu~ 后台',
+		movie:{
+			title:'',
+			doctor:'',
+			country:'',
+			year:'',
+			poster:'',
+			falsh:'',
+			summary:'',
+			language:''
+		}
 	});
 });
 
 // list page
 app.get('/admin/list',function(x,y){
 	y.render('list',{
-		title:'biu~ 列表页'
+		title:'biu~ 列表页',
+		movies:[{
+			title
+		}]
 	});
 });
 
